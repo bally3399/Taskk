3cf09204bc8da8ddcefcbb36b0f50f9045f55403
Baliqis Sulaiman <151347697+bally3399@users.noreply.github.com>
2024-03-07 08:54:54 +0100

    Update BackToSenderTest.java

--- BackToSenderTest.java
+++ BackToSenderTest.java
@@ -1,34 +1,28 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BackToSenderTest {
-    private BackToSender backToSender;
-    @BeforeEach
-    public void initializer(){
-        backToSender = new BackToSender();
-    }
     @Test
     public void testWhenCollectionRateIsLessThan50Percentage(){
-        double percentageRate = backToSender.percentageRate(80);
-        double ridersDailyWage = backToSender.calculateDailyWage(percentageRate);
+        double percentageRate = BackToSender.percentageRate(80);
+        double ridersDailyWage = BackToSender.calculateDailyWage(percentageRate);
         assertEquals(45000, ridersDailyWage);
     }
 
     @Test
     public void testWhenCollectionRateIsBetween50To69Percentage(){
-        double percentageRate = backToSender.percentageRate(25);
-        double ridersDailyWage = backToSender.calculateDailyWage(percentageRate);
+        double percentageRate = BackToSender.percentageRate(25);
+        double ridersDailyWage = BackToSender.calculateDailyWage(percentageRate);
         assertEquals(9000, ridersDailyWage);
     }
     @Test
     public void testWhenCollectionRateIsAboveTheRate(){
-        assertThrows(IllegalArgumentException.class,()-> backToSender.calculateDailyWage(120));
+        assertThrows(IllegalArgumentException.class,()-> BackToSender.calculateDailyWage(120));
     }
     @Test
     public void testWhenCollectionRateIsNegative(){
-        assertThrows(IllegalArgumentException.class,()-> backToSender.calculateDailyWage(-50));
+        assertThrows(IllegalArgumentException.class,()-> BackToSender.calculateDailyWage(-50));
     }
 
 }
